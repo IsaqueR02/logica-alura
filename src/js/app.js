68d9db7c9ced639e8b26a02b116aed1d6cfa42d3
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2025-08-11 20:17:23 +0000

    Adicionando regras de proteção

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,14 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+    return;
+    }
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
+    }
     /**
      * Array para inserir os números que retornarão
      */
