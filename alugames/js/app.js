491fc701dad20360d032d9880e633c46caad1a6d
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2025-08-16 21:58:36 +0000

    Corrijindo a lógica de confirmação do alugames

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,9 +1,10 @@
-function alterarStatus(id) {
-    let jogosAlugados = 0;
+let jogosAlugados = 0;
 
-    function contarEExibirJogosAlugados() {
+function contarEExibirJogosAlugados() {
         console.log(`Total de jogos alugados: ${jogosAlugados}`);
     }
+
+function alterarStatus(id) {
 
     let gameClick = document.getElementById(`game-${id}`);
     let imagem = gameClick.querySelector('.dashboard__item__img');
@@ -13,14 +14,18 @@ function alterarStatus(id) {
     /**
      * Confirmação antes de devolver o jogo
      */
-    if (confirm(imagem.classList.contains('dashboard__item__img--rented'))) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        button.classList.remove('dashboard__item__button--return')
-        button.textContent = 'Alugar';
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)){
+            imagem.classList.remove('dashboard__item__img--rented');
+            button.classList.remove('dashboard__item__button--return')
+            button.textContent = 'Alugar';
+        }
+        jogosAlugados--;
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         button.classList.add('dashboard__item__button--return');
         button.textContent = 'Devolver';
+        jogosAlugados++;
     }
 
     contarEExibirJogosAlugados();
