e70db70a7c859927cdb124f1b823045534287385
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2025-08-16 17:08:57 +0000

    corrigindo erros lógicos

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,42 +1,26 @@
 let totalGeral;
-document.getElementById('lista-produtos').innerHTML = '';
-document.getElementById('valor-total').textContent = 'R$ 0'
-
 limpar();
 
 function adicionar() {
     /**
      * Recuperar os valores nome do produto, quantidade e valor
      */
-    let nameProduct = document.getElementById('produto')
-    let qtndProduct = document.getElementById('quantidade')
-
-    // Verificar se o produto selecionado é válido
-    if (!produto || produto.trim() === "") {
-        alert("Selecione um produto válido.");
-        return;
-    }
-
-
-    // Verificar se a quantidade inserida é válida
-    if (isNaN(quantidade) || quantidade <= 0) {
-        alert("Insira uma quantidade válida.");
-        return;
-    }
-    
+    let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnit = produto.split('R$')[1];
+    let qtndProduct = document.getElementById('quantidade').value;
     /**
       * calcular o preço sendo o nosso subtotal
       */
-    let preco = qtndProduct.value * valorUnit;
-
+    let preco = qtndProduct * valorUnit;
     let carrinho = document.getElementById('lista-produtos');
     /** 
       * adicionar no carrinho
       * */
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${qtndProduct}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
+     carrinho.innerHTML = carrinho.innerHTML + `
+  <section
+    class="carrinho__produtos__produto">
+    <span class="texto-azul">${qtndProduct}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
   </section>`;
     /** 
       * atualizar o valor total
@@ -45,6 +29,18 @@ function adicionar() {
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
     document.getElementById('quantidade').value = 0;
+
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(qtndProduct) || qtndProduct <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
 }
 
 function limpar() {
